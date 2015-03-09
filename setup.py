026ed600c3a7448e8566d98891ff34d8559be6d6
Ben Whalley <benwhalley@gmail.con>
2015-03-09 14:51:14 +0000

    version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author='Ben Whalley',
     author_email='benwhalley@gmail.com',
     url='http://pypi.python.org/pypi/fab-controller/',
-    version="0.9.12",
+    version="0.9.14",
     license='LICENSE.txt',
 
     scripts = ['bin/fab', ],
